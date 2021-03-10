c006bcea74e7b480031b9b471705d6efb3fec945
kazemiai <sara.kazemi@achieveinternet.com>
2021-03-10 10:06:29 -0800

    documentation

--- src/user/user.route.js
+++ src/user/user.route.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("./user.controller");
-const { userService } = require("./dependency");
+const express = require("express");
+const router = express.Router();
+const UserController = require("./user.controller");
+const UserRepository = require("./user.repository");
+const UserService = require("./user.service");
+
+const userRepository = new UserRepository();
+const userService = new UserService(userRepository);
+module.exports = {
+  userRepository,
+  userService
+};
+
+const userController = new UserController(userService);
+
+router.post("/user", (req, res) => userController.register(req, res));
+router.get("/user/:id", (req, res) => userController.getUser(req, res));
+
+module.exports = router;
 
 const userController = new UserController(userService);
 
